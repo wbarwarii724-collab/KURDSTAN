d6743ced52f64a381399f7860650e7290031c0cb
wbarwarii724-collab <wbarwarii724@gmail.com>
2026-07-08 11:51:45 +0300

    Refactor TelegramClient setup and message forwarding
    Updated TelegramClient initialization and message handling logic.

--- bot.py
+++ bot.py
@@ -1,21 +1,22 @@
-from telethon import TelegramClient
+from telethon import TelegramClient, events
 import asyncio
+from telethon.sessions import StringSession
 
 # ========== ڕێکخستنەکان ==========
 api_id = 33790522
 api_hash = "00e4131295f55452e143c06099c1ddae"
-phone = "+96407504399022"
+phone = "+96407504399022"  # ئەمە بگۆڕە بۆ ژمارەی مۆبایلی ڕاست
 # ===================================
 
-SOURCE_CHANNEL = "@WarnisxCcScrap"   # کەناڵی سەرچاوە
-TARGET_CHANNEL = "@Cc428Kurd"        # کەناڵی خۆت
+SOURCE_CHANNEL = "@WarnisxCcScrap"
+TARGET_CHANNEL = "@Cc428Kurd"
 
-client = TelegramClient(phone, api_id, api_hash)
+client = TelegramClient(StringSession(), api_id, api_hash)
 
 async def main():
-    await client.start()
+    await client.start(phone)
     print("✅ سەرکەوتووانە چووە ناوەوە!")
-    
+
     async for message in client.iter_messages(SOURCE_CHANNEL, limit=1):
         if message.text:
             await client.send_message(TARGET_CHANNEL, message.text)
